4d76de920e26e39161a31f29aea5ca4a39d0a99f
kdev0x <119963213+kdev0x@users.noreply.github.com>
2022-12-31 10:57:21 +0000

    Create code-038.asm

---
+++ code-038.asm
@@ -0,0 +1,19 @@
+;((1 + 2 + 3 - 1 * 2) ^ ((5 + 1) / 2) >> (8 | 12)
+mov a, 2
+mov b, 1
+mul 1
+sub a, 3
+add a, 2
+add b, a
+
+mov a, 5
+add a, 1
+mov c, 2
+div c
+
+xor b, a
+mov d, 8
+or d, 12
+shr b, d
+
+
