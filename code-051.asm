66586b53969af6fb10fa7101de192be577a580cf
kdev0x <119963213+kdev0x@users.noreply.github.com>
2023-01-21 11:53:55 +0000

    Create code-051.asm

---
+++ code-051.asm
@@ -0,0 +1,20 @@
+jmp code
+message: db "Visual Studio Code>Pycharm "
+         db 0
+code:
+mov a, message 
+call len
+   HLT
+
+
+len:
+mov c, 0
+loop:
+   mov d, [a]
+   cmp d, 0
+   je end_of_loop
+   inc c
+   inc a
+   jmp loop
+   end_of_loop:
+   RET
